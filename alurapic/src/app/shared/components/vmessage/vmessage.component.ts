15cd9323f9a39027702f04fc9eece7bcf5f482e7
Alan Brito <alanbritodossantos2015@gmail.com>
2021-05-31 00:40:26 -0300

    teste realizado vs code

--- alurapic/src/app/shared/components/vmessage/vmessage.component.ts
+++ alurapic/src/app/shared/components/vmessage/vmessage.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'ap-vmessage',
+  selector: 'ap-vmessage',//seletor
   templateUrl: './vmessage.component.html'
 })
 export class VMessageComponent {
