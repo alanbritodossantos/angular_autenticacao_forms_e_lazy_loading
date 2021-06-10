5cc53fd29a5561703a73e4766f48b6bbb1ad0a24
Alan Brito <alanbritodossantos2015@gmail.com>
2021-06-10 00:28:09 -0300

    Implementação do logout

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -12,9 +13,16 @@ export class HeaderComponent {
   // a variavel com tqm "$" na frente significa dizer que esse valor recebe um observable
   user$: Observable<User>;
 
-  constructor(userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router:Router) {
 
     this.user$ = userService.getUser();
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 
 }
