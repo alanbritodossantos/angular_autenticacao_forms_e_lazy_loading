ce6bee0336bf0c0e4f05dde0ce5a5115be4d3d17
Alan Brito <alanbritodossantos2015@gmail.com>
2021-06-09 22:29:28 -0300

    O papel do BehaviorSubject

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector:'ap-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  // a variavel com tqm "$" na frente significa dizer que esse valor recebe um observable
+  user$: Observable<User>;
+  user: User;
+
+  constructor(userService: UserService) {
+
+    this.user$ = userService.getUser();
+    this.user$.subscribe(user => this.user = user);
+  }
 
 }
